5a8fa9df458c462d775223268d6409fd9e004603
Hung Nguyen <hungnguyen@Hungs-Hackintosh.local>
2019-07-19 22:43:44 -0400

    update menu-items to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,8 @@
-import './homepage.styles.scss';
 import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
