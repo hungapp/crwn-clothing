a8cb64dc2d18cb67dfd2126e687edebea15d80ff
Hung Nguyen <hungnguyen@Hungs-Hackintosh.local>
2019-08-09 19:32:52 -0400

    added express server to help handle backend strip payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_VmCtOmywghv8VnrISXMPyItX00pM2Xj8qm';
+
   const onToken = token => {
-    console.log(token);
-    alert('Payment Sucessful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card info'
+        );
+      });
   };
 
   return (
